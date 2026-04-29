5714f414e34540e4a7041a9f0f5f9edeb720f6c8
eric <ericpredit@gmail.com>
2026-04-29 22:23:05 +0300

    created logout handler to frontend

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,9 +1,22 @@
+import { createSupabaseBrowserClient } from "@/lib/supabase/client";
+import { create } from "domain";
+import { useRouter } from "next/navigation";
+
 export default function SettingsPage() {
-   return(
-   <div className="h-full flex justify-center items-center">
-    <button className=" bg-red-600">
-        Log Out
-    </button>
-    </div>
-    )
+    const router = useRouter();
+    const supabase = createSupabaseBrowserClient();
+
+    const handleLogout = async () => {
+        await supabase.auth.signOut();
+        router.push("/login");
+        router.refresh();
+    };
+
+    return (
+        <div className="h-full flex justify-center items-center">
+            <button onClick={handleLogout} className=" bg-red-600">
+                Log Out
+            </button>
+        </div>
+    );
 }
